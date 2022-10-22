127e0498d8643a6d954a7a32111ff9ee46e35f2b
camilabattistini <camibattistini@gmail.com>
2022-10-22 09:58:30 -0300

    mejoras para responsividad y funciones

--- script.js
+++ script.js
@@ -7,40 +7,45 @@ var copiar = document.querySelector(".copiar");
 var muñeco = document.querySelector(".muñeco");
 var tituloResultado = document.querySelector(".tituloResultado");
 var textoResultado = document.querySelector(".textoResultado");
-
-
+var texto = "";
 
 
 //ENCRIPTANDO EL TEXTO
 
 function encriptarTexto() {
-    var texto = document.querySelector(".ingresarTexto").value;
-    textoEncriptado = texto.replaceAll("a","ai");
-    textoEncriptado = textoEncriptado.replaceAll("e","enter");
-    textoEncriptado = textoEncriptado.replaceAll("i","imes");
-    textoEncriptado = textoEncriptado.replaceAll("o","ober");
-    textoEncriptado = textoEncriptado.replaceAll("u","ufat");
-    mostrarResultadoEncriptado(textoResultadoLugar);
+    texto = document.querySelector(".ingresarTexto").value;
+    let matrizCodigo = [['e', 'enter'], ['i', 'imes'], ['o', 'ober'], ['a', 'ai'], ['u', 'ufat']];
+    texto = texto.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+        if(texto.includes(matrizCodigo[i][0])) {
+            texto = texto.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1]);
+        }
+    }
+    mostrarResultado(textoResultadoLugar);
 }
 
 
 //DESENCRIPTANDO EL TEXTO
 
 function desencriptarTexto() {
-    var texto = document.querySelector(".ingresarTexto").value;
-    textoDesencriptado = texto.replaceAll("ufat","u");
-    textoDesencriptado = textoDesencriptado.replaceAll("ober","o");
-    textoDesencriptado = textoDesencriptado.replaceAll("imes","i");
-    textoDesencriptado = textoDesencriptado.replaceAll("enter","e");
-    textoDesencriptado = textoDesencriptado.replaceAll("ai","a");
-    mostrarResultadoDesencriptado(textoResultadoLugar);
+    texto = document.querySelector(".ingresarTexto").value;
+    let matrizCodigo = [['e', 'enter'], ['i', 'imes'], ['o', 'ober'], ['a', 'ai'], ['u', 'ufat']];
+    texto = texto.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+        if(texto.includes(matrizCodigo[i][1])) {
+            texto = texto.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0]);
+        }
+    }
+    mostrarResultado(textoResultadoLugar);
 }
 
 
 //MOSTRANDO EL RESULTADO POR PANTALLA
 
-function mostrarResultadoEncriptado(resultado) {
-    resultado.innerHTML = textoEncriptado;
+function mostrarResultado(resultado) {
+    resultado.innerHTML = texto;
     textoResultadoLugar.style.display = 'block';
     copiar.style.display = 'block';
     muñeco.style.display = 'none';
@@ -48,24 +53,9 @@ function mostrarResultadoEncriptado(resultado) {
     textoResultado.style.display = 'none';
 }
 
-function mostrarResultadoDesencriptado(resultado) {
-    resultado.innerHTML = textoDesencriptado;
-    copiar.style.display = 'block';
-    muñeco.style.display = 'none';
-    tituloResultado.style.display = 'none';
-    textoResultado.style.display = 'none';
-}
+
 
 //COPIANDO EL RESULTADO
 function copiarTexto() {
-    var content = document.getElementsByClassName('textoResultadoLugar').innerHTML;
-    
-    navigator.clipboard.writeText(content)
-        .then(() => {
-        console.log("Text copied to clipboard...")
-    })
-        .catch(err => {
-        console.log('Something went wrong', err);
-    })
-    console.log(content);
+    navigator.clipboard.writeText(texto);
 }
